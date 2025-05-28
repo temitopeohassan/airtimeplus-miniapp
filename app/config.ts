a25fd5d619a7c78f962a734f470fdbf9f87c43bd
temitopeohassan <temitope.hassan.2@gmail.com>
2025-05-28 17:32:02 +0100

    update config file url

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://airtimeplus-api.vercel.app';
 
 // API endpoints
 export const API_ENDPOINTS = {
